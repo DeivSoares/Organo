abeddb640f66f6a2b1f4f459d54c965239786e21
Deivison Soares <deivisonsoares1610@outlook.com>
2023-08-05 06:53:34 -0300

    Update index.js

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -7,23 +7,23 @@ const Rodape = () => {
             <ul>
                 <li>
                     <a href="facebook.com" target="_blank">
-                        <img src="images/fb.png" alt="" />
+                        <img src="images/fb.png" alt="Logo do Facebook" />
                     </a>
                 </li>
                 <li>
                     <a href="twitter.com" target="_blank">
-                        <img src="/images/tw.png" alt="" />
+                        <img src="/images/tw.png" alt="Logo do Twitter" />
                     </a>
                 </li>
                 <li>
-                    <a href="instagram.com" target="_blank">
-                        <img src="/images/ig.png" alt="" />
+                    <a href="instagram.com/deiv_soares" target="_blank">
+                        <img src="/images/ig.png" alt="Logo do Instagram" />
                     </a>
                 </li>
             </ul>
         </section>
         <section>
-            <img src="/images/logo.png" alt="" />
+            <img src="/images/logo.png" alt="Logo Organo" />
         </section>
         <section>
             <p>
